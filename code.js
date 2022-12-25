0462841f04ac4700bd685d642ceb8616719f1e8e
M3phX666 <117993300+M3phX666@users.noreply.github.com>
2022-12-25 16:47:26 +0100

    fixed border

--- code.js
+++ code.js
@@ -1,10 +1,19 @@
 function darkTheme(e) {
     document.body.classList = "bg-dark";
     let pCon = document.getElementById("postContainer").style.color = "white";
+    let cards = document.getElementsByClassName("card");
+    for(var i = 0; i < cards.length; i++) {
+        cards[i].style.border = "1px solid rgba(0,0,0,.875)";
+    }
+    console.log(cards);
     e.preventDefault();
 }
 function lightTheme(e) {
     document.body.classList = "bg-light";
     let pCon = document.getElementById("postContainer").style.color = "black";
+    let cards = document.getElementsByClassName("card");
+    for(var i = 0; i < cards.length; i++) {
+        cards[i].style.border = "1px solid rgba(0,0,0,.125)";
+    }
     e.preventDefault();
 }
