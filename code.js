c4d443aac70526adee3080979434893b056b7abb
M3phX666 <117993300+M3phX666@users.noreply.github.com>
2022-12-25 14:40:05 +0100

    update

---
+++ code.js
@@ -0,0 +1,8 @@
+function darkTheme(e) {
+    document.body.classList = "bg-dark";
+    e.preventDefault();
+}
+function lightTheme(e) {
+    document.body.classList = "";
+    e.preventDefault();
+}
