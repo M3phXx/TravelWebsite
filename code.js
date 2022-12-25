83807a543fc3087be188a43128b0de1336fb7800
M3phX666 <117993300+M3phX666@users.noreply.github.com>
2022-12-25 15:41:24 +0100

    fixed font color problem

--- code.js
+++ code.js
@@ -1,8 +1,10 @@
 function darkTheme(e) {
     document.body.classList = "bg-dark";
+    let pCon = document.getElementById("postContainer").style.color = "white";
     e.preventDefault();
 }
 function lightTheme(e) {
     document.body.classList = "bg-light";
+    let pCon = document.getElementById("postContainer").style.color = "black";
     e.preventDefault();
 }
