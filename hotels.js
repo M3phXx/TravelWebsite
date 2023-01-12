cbb1bf609fb007381188c752e5ad011e17de4f3d
M3phX666 <117993300+M3phX666@users.noreply.github.com>
2023-01-12 02:48:11 +0100

    hotels-CountingNumbers

--- hotels.js
+++ hotels.js
@@ -31,3 +31,42 @@ function setTheme() {
     console.log(JSON.parse(localStorage.getItem("theme")));
     document.body.style.backgroundColor = `rgb(${theme[1]}, ${theme[1]}, ${theme[1]})`
 }
+/* --------------------------------------Counter-------------------------------------- */
+let section_counter = document.querySelector('#section_counter');
+let counters = document.querySelectorAll('.counter-item .counter');
+
+let CounterObserver = new IntersectionObserver(
+  (entries, observer) => {
+    let [entry] = entries;
+    if (!entry.isIntersecting) return;
+
+    let speed = 60;
+    counters.forEach((counter, index) => {
+      function UpdateCounter() {
+        const targetNumber = +counter.dataset.target;
+        const initialNumber = +counter.innerText;
+        const incPerCount = targetNumber / speed;
+        if (initialNumber < targetNumber) {
+          counter.innerText = Math.ceil(initialNumber + incPerCount);
+          setTimeout(UpdateCounter, 40);
+        }
+      }
+      UpdateCounter();
+
+      if (counter.parentElement.style.animation) {
+        counter.parentElement.style.animation = '';
+      } else {
+        counter.parentElement.style.animation = `slide-up 0.3s ease forwards ${
+          index / counters.length + 0.5
+        }s`;
+      }
+    });
+    observer.unobserve(section_counter);
+  },
+  {
+    root: null,
+    threshold: window.innerWidth > 768 ? 0.4 : 0.3,
+  }
+);
+
+CounterObserver.observe(section_counter);
